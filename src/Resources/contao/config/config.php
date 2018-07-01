d6b4b55eabdcd5fc4e1b95b5eaed9ab10a437922
Alexander Schwirjow <alexander@schwirjow.de>
2018-07-01 22:32:49 +0200

    Update the hook

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -2,5 +2,5 @@
 
 array_insert($GLOBALS['BE_MOD']['design'], 4, array('fonts' => array('tables' => array('tl_fonts_faces', 'tl_fonts'))));
 
-$GLOBALS['TL_HOOKS']['generatePage'][] = array('SPoT\\WebfontsBundle\\FontFaces', 'generatePageHook');
+$GLOBALS['TL_HOOKS']['generatePage'][] = array('SPoT\\WebfontsGeneratorBundle\\FontFaces', 'generatePageHook');
 
