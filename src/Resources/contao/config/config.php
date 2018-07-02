0ab1ee7213f21cd8a537c41afc3c132fb8fc6576
Alexander Schwirjow <alexanderschwirjow@iMac-020-Alexander.local>
2018-07-02 13:06:55 +0200

    Fix syntax error

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['BE_MOD']['design'][] = array('fonts' => array('tables' => array('tl_fonts_faces', 'tl_fonts'));
+$GLOBALS['BE_MOD']['design'][] = array('fonts' => array('tables' => array('tl_fonts_faces', 'tl_fonts')));
 $GLOBALS['BE_MOD']['design']['themes']['tables'][] = 'tl_fonts_faces';
 
 $GLOBALS['TL_HOOKS']['generatePage'][] = array('SPoT\\WebfontGeneratorBundle\\FontFaces', 'generatePageHook');
