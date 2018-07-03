7145d355060c1109443251646934c25cff55b796
Alexander Schwirjow <alexanderschwirjow@iMac-020-Alexander.local>
2018-07-03 13:33:33 +0200

    Leave the class where it is for now

--- src/Resources/contao/dca/tl_fonts.php
+++ src/Resources/contao/dca/tl_fonts.php
@@ -40,9 +40,7 @@ $GLOBALS['TL_DCA']['tl_fonts'] = array
         (
             'fields'                  => array('weight'),
             'format'                  => '%s',
-            'label_callback'          => array(
-                array('SPoT\\WebfontGeneratorBundle\\FontStyles', 'renderStyleLabel'),
-            ),
+            'label_callback'          => array('tl_fonts', 'renderStylelabel'),
         ),
         'global_operations' => array
         (
@@ -265,3 +263,42 @@ $GLOBALS['TL_DCA']['tl_fonts'] = array
         ),
     )
 );
+
+class tl_fonts extends \Backend
+{
+    public function renderStyleLabel($row, $label)
+    {
+        if ($row['stretch'] !== 'normal') {
+            $label .= ',&nbsp;'.$row['stretch'];
+        }
+
+        $format = array();
+        if ($row['src_ttf']) $format[] = 'ttf';
+        if ($row['src_otf']) $format[] = 'otf';
+        if ($row['src_woff']) $format[] = 'woff';
+        if ($row['src_woff_two']) $format[] = 'woff2';
+        if ($row['src_svg']) $format[] = 'svg';
+        if ($row['src_eot']) $format[] = 'eot';
+
+        if ($format) {
+            $format = implode(', ', $format);
+            $label = sprintf('<span style="color:#b3b3b3;padding-left:3px">[%s]</span>%s %s', $format, $this->getFontFaceName($row['pid']), $label);
+        }
+
+        if ($row['style'] !== 'normal') {
+            $label = sprintf('<i>%s, %s</i>', $label, $row['style']);
+        }
+
+        return $label;
+    }
+
+    private function getFontFaceName($fontId)
+    {
+        $fontFace = $this->Database->prepare('SELECT name FROM tl_fonts_faces WHERE id = ? LIMIT 1')->execute($fontId);
+        if ($fontFace->name) {
+            return ' '.$fontFace->name;
+        }
+
+        return '';
+    }
+}
