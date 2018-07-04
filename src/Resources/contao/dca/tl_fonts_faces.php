935d4e1ceec63b9711a0dfb13fe9065db7491523
Alexander Schwirjow <alexanderschwirjow@iMac-020-Alexander.local>
2018-07-04 16:12:12 +0200

    Make fonts theme-independent

--- src/Resources/contao/dca/tl_fonts_faces.php
+++ src/Resources/contao/dca/tl_fonts_faces.php
@@ -11,16 +11,15 @@ $GLOBALS['TL_DCA']['tl_fonts_faces'] = array
     // Config
     'config' => array
     (
-        'dataContainer'    => 'Table',
-        'enableVersioning' => false,
-        'ptable'           => 'tl_theme',
-        'ctable'           => array('tl_fonts'),
-        'switchToEdit'     => true,
+        'dataContainer'               => 'Table',
+        'enableVersioning'            => false,
+        'ctable'                      => array('tl_fonts'),
+        'switchToEdit'                => true,
         'sql' => array
         (
             'keys' => array
             (
-                'id' => 'primary',
+                'id'                  => 'primary',
             ),
         ),
         'onsubmit_callback' => array(
@@ -32,59 +31,59 @@ $GLOBALS['TL_DCA']['tl_fonts_faces'] = array
     (
         'sorting' => array
         (
-            'mode'                  => 4,
-            'fields'                => array('name'),
-            'panelLayout'           => 'sort,filter;search,limit',
-            'child_record_callback' => array('tl_fonts_faces', 'listFontStyles')
+            'mode'                    => 1,
+            'fields'                  => array('name'),
+            'panelLayout'             => 'sort,filter;search,limit',
+            'child_record_callback'   => array('tl_fonts_faces', 'listFontStyles')
         ),
         'label' => array
         (
-            'fields' => array('name'),
-            'format' => '%s',
+            'fields'                  => array('name'),
+            'format'                  => '%s',
         ),
         'global_operations' => array
         (
             'all' => array
             (
-                'label'      => &$GLOBALS['TL_LANG']['MSC']['all'],
-                'href'       => 'act=select',
-                'class'      => 'header_edit_all',
-                'attributes' => 'onclick="Backend.getScrollOffset()" accesskey="e"',
+                'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
+                'href'                => 'act=select',
+                'class'               => 'header_edit_all',
+                'attributes'          => 'onclick="Backend.getScrollOffset()" accesskey="e"',
             )
         ),
         'operations' => array
         (
             'edit' => array
             (
-                'label'           => &$GLOBALS['TL_LANG']['tl_fonts_faces']['edit'],
-                'href'            => 'table=tl_fonts',
-                'icon'            => 'edit.gif',
+                'label'               => &$GLOBALS['TL_LANG']['tl_fonts_faces']['edit'],
+                'href'                => 'table=tl_fonts',
+                'icon'                => 'edit.gif',
             ),
             'editheader' => array
             (
-                'label'           => &$GLOBALS['TL_LANG']['tl_fonts_faces']['editheader'],
-                'href'            => 'table=tl_fonts_faces&amp;act=edit',
-                'icon'            => 'header.gif',
-                'button_callback' => array('tl_fonts_faces', 'editHeader'),
+                'label'               => &$GLOBALS['TL_LANG']['tl_fonts_faces']['editheader'],
+                'href'                => 'table=tl_fonts_faces&amp;act=edit',
+                'icon'                => 'header.gif',
+                'button_callback'     => array('tl_fonts_faces', 'editHeader'),
             ),
             'copy' => array
             (
-                'label'           => &$GLOBALS['TL_LANG']['tl_fonts_faces']['copy'],
-                'href'            => 'act=paste&amp;mode=copy',
-                'icon'            => 'copy.gif',
+                'label'               => &$GLOBALS['TL_LANG']['tl_fonts_faces']['copy'],
+                'href'                => 'act=paste&amp;mode=copy',
+                'icon'                => 'copy.gif',
             ),
             'delete' => array
             (
-                'label'           => &$GLOBALS['TL_LANG']['tl_fonts_faces']['delete'],
-                'href'            => 'act=delete',
-                'icon'            => 'delete.gif',
-                'attributes'      => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"',
+                'label'               => &$GLOBALS['TL_LANG']['tl_fonts_faces']['delete'],
+                'href'                => 'act=delete',
+                'icon'                => 'delete.gif',
+                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"',
             ),
             'show' => array
             (
-                'label'           => &$GLOBALS['TL_LANG']['tl_fonts_faces']['show'],
-                'href'            => 'act=show',
-                'icon'            => 'show.gif',
+                'label'               => &$GLOBALS['TL_LANG']['tl_fonts_faces']['show'],
+                'href'                => 'act=show',
+                'icon'                => 'show.gif',
             ),
         )
     ),
@@ -100,38 +99,32 @@ $GLOBALS['TL_DCA']['tl_fonts_faces'] = array
         (
             'sql'                     => "int(10) unsigned NOT NULL auto_increment"
         ),
-        'pid' => array
-		(
-			'foreignKey'              => 'tl_theme.name',
-			'sql'                     => "int(10) unsigned NOT NULL default '0'",
-			'relation'                => array('type' => 'belongsTo', 'load' => 'lazy'),
-		),
         'tstamp' => array
         (
             'sql'                     => "int(10) unsigned NOT NULL default '0'"
         ),
         'name' => array
         (
-            'label'     => &$GLOBALS['TL_LANG']['tl_fonts_faces']['name'],
-            'inputType' => 'text',
-            'search'    => true,
-            'flag'      => 1,
-            'eval'      => array(
-                'mandatory' => true,
-                'unique'    => true,
-                'rgxp'      => 'alnum',
-                'maxlength' => 256,
-                'tl_class'  => 'w50',
-                'doNotCopy' => true,
+            'label'                   => &$GLOBALS['TL_LANG']['tl_fonts_faces']['name'],
+            'inputType'               => 'text',
+            'search'                  => true,
+            'flag'                    => 1,
+            'eval'                    => array(
+                'mandatory'           => true,
+                'unique'              => true,
+                'rgxp'                => 'alnum',
+                'maxlength'           => 256,
+                'tl_class'            => 'w50',
+                'doNotCopy'           => true,
             ),
-            'sql'       => "VARCHAR(256) NULL"
+            'sql'                     => "VARCHAR(256) NULL"
         ),
         'fallback' => array
         (
-            'label'     => &$GLOBALS['TL_LANG']['tl_fonts_faces']['fallback'],
-            'inputType' => 'text',
-            'eval'      => array('mandatory' => true, 'maxlength' => 256, 'tl_class' => 'w50'),
-            'sql'       => "VARCHAR(256) default ''"
+            'label'                   => &$GLOBALS['TL_LANG']['tl_fonts_faces']['fallback'],
+            'inputType'               => 'text',
+            'eval'                    => array('mandatory' => true, 'maxlength' => 256, 'tl_class' => 'w50'),
+            'sql'                     => "VARCHAR(256) default ''"
         ),
     )
 );
@@ -155,10 +148,5 @@ class tl_fonts_faces extends Backend
     public function editHeader($row, $href, $label, $title, $icon, $attributes)
     {
         return $this->User->canEditFieldsOf('tl_fonts_faces') ? '<a href="'.$this->addToUrl($href.'&amp;id='.$row['id']).'" title="'.StringUtil::specialchars($title).'"'.$attributes.'>'.Image::getHtml($icon, $label).'</a> ' : Image::getHtml(preg_replace('/\.svg$/i', '_.svg', $icon)).' ';
-    }
-
-    public function copyFont()
-    {
-
     }
 }
