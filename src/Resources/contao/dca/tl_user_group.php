0ebafdd57fbf81aa3a5a25578048e0a68b6f10c5
Alexander Schwirjow <alexanderschwirjow@iMac-020-Alexander.local>
2018-07-06 13:58:44 +0200

    Fix transtaltions

--- src/Resources/contao/dca/tl_user_group.php
+++ src/Resources/contao/dca/tl_user_group.php
@@ -3,7 +3,7 @@
 $GLOBALS['TL_DCA']['tl_user_group']['palettes']['default'] .= ';{webfont_legend},webfont_generator';
 
 $GLOBALS['TL_DCA']['tl_user_group']['fields']['webfont_generator'] = array(
-    'label'                   => &$GLOBALS['TL_LANG']['tl_user']['webfont_generator'],
+    'label'                   => &$GLOBALS['TL_LANG']['tl_user_group']['webfont_generator'],
     'exclude'                 => true,
     'inputType'               => 'checkbox',
     'options'                 => array('create', 'delete'),
