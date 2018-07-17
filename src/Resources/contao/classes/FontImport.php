988f2f22a2cbd5e704db910aaa650c24793d1ea0
Alexander Schwirjow <alexanderschwirjow@iMac-020-Alexander.local>
2018-07-17 12:18:25 +0200

    prepare import

--- src/Resources/contao/classes/FontImport.php
+++ src/Resources/contao/classes/FontImport.php
@@ -20,6 +20,7 @@ class FontImport extends \Backend
 
         if (\Input::post('FORM_SUBMIT') == 'tl_font_import') {
             $arrUploaded = $objUploader->uploadTo('system/tmp');
+            $fontIds = array();
 
             if (empty($arrUploaded)) {
                 \Message::addError($GLOBALS['TL_LANG']['ERR']['all_fields']);
@@ -44,7 +45,69 @@ class FontImport extends \Backend
                 $strFile = $objFile->getContent();
                 $strFile = str_replace("\r", '', $strFile);
 
-                \Message::addConfirmation(sprintf($GLOBALS['TL_LANG']['tl_fonts_faces']['css_imported'], $strCssFile.'.css'));
+                $fontData = array();
+
+                foreach ($strFile as $font) {
+                    $fontData[] = array(
+                        'name'   => $this->parseRule($font, 'font\-family'),
+                        'weight' => $this->parseRule($font, 'font\-weight'),
+                        'style'  => $this->parseRule($font, 'font\-style'),
+                        'src'    => $this->parseFontSources($font),
+                    );
+                }
+
+                foreach ($output as $font) {
+                    $query = 'INSERT INTO tl_fonts (%s) VALUES (?)';
+                    $keys = array();
+                    $values = array();
+
+                    if ($font['name']) {
+                        $keys[] = 'name';
+                        $values[] = $font['name'];
+                    }
+                    if ($font['weight']) {
+                        $keys[] = 'weight';
+                        $values[] = $font['weight'];
+                    }
+                    if ($font['style']) {
+                        $keys[] = 'style';
+                        $values[] = $font['style'];
+                    }
+                    if ($font['src']['truetype']) {
+                        $keys[] = 'src_ttf';
+                        $values[] = $font['src']['truetype'];
+                    }
+                    if ($font['src']['opentype']) {
+                        $keys[] = 'src_otf';
+                        $values[] = $font['src']['opentype'];
+                    }
+                    if ($font['src']['woff']) {
+                        $keys[] = 'src_woff';
+                        $values[] = $font['src']['woff'];
+                    }
+                    if ($font['src']['woff2']) {
+                        $keys[] = 'src_woff_two';
+                        $values[] = $font['src']['woff2'];
+                    }
+                    if ($font['src']['svg']) {
+                        $keys[] = 'src_svg';
+                        $values[] = $font['src']['svg'];
+                    }
+                    if ($font['src']['embedded-opentype']) {
+                        $keys[] = 'src_eot';
+                        $values[] = $font['src']['embedded-opentype'];
+                    }
+
+                    $query = sprintf($query, implode(',', $keys));
+
+                    $result = $this->Database->prepare($query)->execute($values);
+
+                    if ($result->id) {
+                        $fontIds[] = $result->id;
+                    }
+                }
+
+                \Message::addConfirmation(sprintf($GLOBALS['TL_LANG']['tl_fonts_faces']['css_imported'], implode(',', $fontIds).'.css'));
             }
 
             \System::setCookie('BE_PAGE_OFFSET', 0, 0);
@@ -84,7 +147,7 @@ class FontImport extends \Backend
         $sources = array();
         foreach ($fontSources as $fontSource) {
             if (isset($fontSource[1], $fontSource[2])) {
-                $sources[] = array($fontSource[2] => $fontSource[1]);
+                $sources[$fontSource[2]] = $fontSource[1];
             }
         }
 
