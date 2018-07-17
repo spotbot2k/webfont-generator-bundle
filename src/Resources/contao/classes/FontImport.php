03e4282a905399ad7adfb6699bb2e1f23c5aa4a0
Alexander Schwirjow <alexanderschwirjow@iMac-020-Alexander.local>
2018-07-17 08:27:19 +0200

    add base template markup

--- src/Resources/contao/classes/FontImport.php
+++ src/Resources/contao/classes/FontImport.php
@@ -15,13 +15,30 @@ class FontImport extends \BackendModule
 
     public function generate()
     {
-        $this->Template = new \BackendTemplate($this->strTemplate);
+        $template = $this->prepareTemplate();
         $this->compile();
 
-        return $this->Template->parse();
+        return $template->parse();
     }
 
     protected function compile()
     {
+    }
+
+    private function prepareTemplate()
+    {
+        $template = new \BackendTemplate($this->strTemplate);
+        $config = $this->framework->getAdapter(Config::class);
+        $uploader = $this->framework->createInstance(FileUpload::class);
+
+        $template->uploader = $uploader->generateMarkup();
+        $template->fileMaxSize = $config->get('maxFileSize');
+        $template->backBTTitle = $this->translator->trans('MSC.backBTTitle', [], 'contao_default');
+        $template->backBT = $this->translator->trans('MSC.backBT', [], 'contao_default');
+        $template->submitLabel = $this->translator->trans('MSC.apply', [], 'contao_default');
+        $template->sourceLabel = $this->translator->trans('MSC.source.0', [], 'contao_default');
+        $template->sourceLabelHelp = $this->translator->trans('MSC.source.1', [], 'contao_default');
+
+        return $template;
     }
 }
